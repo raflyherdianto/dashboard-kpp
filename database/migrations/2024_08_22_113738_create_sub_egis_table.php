493ec98b6f05e9918c1e6233bee23f90e5ec918a
Shankara21 <lazuardit21@gmail.com>
2024-08-24 19:33:19 +0700

    feat: Add foreign key constraint for egi_id in sub_egis table

--- database/migrations/2024_08_22_113738_create_sub_egis_table.php
+++ database/migrations/2024_08_22_113738_create_sub_egis_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('sub_egis', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('egi_id')->constrained();
             $table->string('name');
             $table->timestamps();
         });
