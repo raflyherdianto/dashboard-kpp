3807cd54212dc0e0dfa9de85f9914025780a2c35
Shankara21 <lazuardit21@gmail.com>
2024-08-31 18:24:53 +0700

    feat: Import data from Excel file in HistoryPelatihanController

--- app/Http/Controllers/HistoryPelatihanController.php
+++ app/Http/Controllers/HistoryPelatihanController.php
@@ -8,7 +8,14 @@ use App\Models\Department;
 use App\Models\HistoryPelatihan;
 use App\Models\Pelatihan;
 use App\Models\Site;
+use App\Models\SubEgi;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
+
 use App\Models\User;
+use Illuminate\Http\Request;
+use RealRashid\SweetAlert\Facades\Alert;
+use Maatwebsite\Excel\Facades\Excel;
+
 
 class HistoryPelatihanController extends Controller
 {
@@ -20,6 +27,55 @@ class HistoryPelatihanController extends Controller
         $datas = HistoryPelatihan::all();
 
         return view('pages.history-pelatihan.index', compact('datas'));
+    }
+
+    public function importData(Request $request)
+    {
+        if ($request->file('excel') == null) {
+            Alert::error('Error', 'File not found');
+            return redirect()->back();
+        }
+        if (!in_array($request->file('excel')->getClientOriginalExtension(), ['xlsx', 'xls', 'csv'])) {
+            Alert::error('Error', 'Please upload file in .xlsx, .xls or .csv format');
+            return redirect()->back();
+        }
+        $mekaniks = User::role('Mekanik')->get();
+        $instrukturs = User::role('Instruktur')->get();
+        $pelatihans = Pelatihan::all();
+        $sites = Site::all();
+        $departments = Department::all();
+        $subEgis = SubEgi::all();
+        $rawFile = Excel::toArray([], $request->file('excel'))[0];
+        unset($rawFile[0]);
+        $datas = [];
+        $lowercaseDepartments = $departments->map(function ($department) {
+            $department->name = strtolower($department->name);
+            return $department;
+        });
+        foreach ($rawFile as $key => $value) {
+            dd($value[9], $subEgis);
+            $datas[] = [
+                // 'mekanik_id' => $mekaniks->where('nrp', $value[0])->first()->id,
+                'site_id' => $sites->where('name', $value[2])->first()->id,
+                'department_id' => $departments->filter(function ($department) use ($value) {
+                    return stripos(strtolower($department->name), strtolower($value[3])) !== false;
+                })->first()->id,
+                'location' => $value[5],
+                'pelatihan_id' => $pelatihans->where('name', $value[6])->first()->id,
+                'start_date' => Date::excelToDateTimeObject($value[7])->format('Y-m-d'),
+                'end_date' =>  Date::excelToDateTimeObject($value[8])->format('Y-m-d'),
+                // 'sub_egi_id' => $subEgis->filter(function ($subEgi) use ($value) {
+                //     return stripos(strtolower($subEgi->name), strtolower('%' . $value[9] . '%')) !== false;
+                // })->first()->id,
+                'instruktur_id' => $value[10] == null ? null : $instrukturs->where('name', $value[10])->first()->id,
+                'status' => $value[11]
+            ];
+
+            dd($datas);
+        }
+        $scores = [];
+        Alert::success("Success", "Data has been added");
+        return redirect()->back();
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\HistoryPelatihanController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MekanikController;
 use App\Http\Controllers\PelatihanController;
-use App\Http\Controllers\UserController;    
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/dashboard');
@@ -31,4 +31,5 @@ Route::middleware(['auth'])->group(function () {
 
     Route::resource('pelatihan', PelatihanController::class);
     Route::resource('history-pelatihan', HistoryPelatihanController::class);
+    Route::post('history-pelatihan/import-data', [HistoryPelatihanController::class, 'importData'])->name('history-pelatihan.import');
 });
